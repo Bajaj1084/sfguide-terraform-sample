31e511b0d8bc0f80220a8e5d288c005abefea0cb
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-14 19:25:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,75 @@ resource "snowflake_warehouse" "warehouse" {
   warehouse_size = "xsmall"
   auto_suspend   = 60
 }
+
+
+resource "snowflake_role" "role" {
+  
+  name     = "TF_DEMO_SVC_ROLE"
+}
+
+resource "snowflake_grant_privileges_to_account_role" "database_grant" {
+  
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = snowflake_database.db.name
+  }
+}
+
+resource "snowflake_schema" "schema" {
+  database   = snowflake_database.db.name
+  name       = "TF_DEMO"
+  is_managed = false
+}
+
+resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
+ 
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_schema {
+    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
+
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_role.role.name
+  on_account_object {
+    object_type = "WAREHOUSE"
+    object_name = snowflake_warehouse.warehouse.name
+  }
+}
+
+resource "tls_private_key" "svc_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "snowflake_user" "user" {
+ 
+    name              = "tf_demo_user"
+    default_warehouse = snowflake_warehouse.warehouse.name
+    default_role      = snowflake_role.role.name
+    default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
+    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
+}
+
+resource "snowflake_grant_privileges_to_account_role" "user_grant" {
+
+  privileges        = ["MONITOR"]
+  account_role_name = snowflake_role.role.name  
+  on_account_object {
+    object_type = "USER"
+    object_name = snowflake_user.user.name
+  }
+}
+
+resource "snowflake_grant_account_role" "grants" {
+
+  role_name = snowflake_role.role.name
+  user_name = snowflake_user.user.name
+}
+
