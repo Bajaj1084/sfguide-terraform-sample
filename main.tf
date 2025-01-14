e4bda3b4e0ed9f0adfa55465d1b3ba99d7fae903
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-01-14 19:38:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,14 +34,10 @@ resource "snowflake_role" "role" {
   name     = "TF_DEMO_SVC_ROLE"
 }
 
-resource "snowflake_grant_privileges_to_account_role" "database_grant" {
-  
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.db.name
-  }
+resource "snowflake_grant" "database_grant" {
+  database_name = "TF_DEMO"
+  privilege     = "USAGE"
+  roles         = ["TF_DEMO_SVC_ROLE"]
 }
 
 resource "snowflake_schema" "schema" {
@@ -50,52 +46,12 @@ resource "snowflake_schema" "schema" {
   is_managed = false
 }
 
-resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
- 
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_schema {
-    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
-  }
+
+resource "snowflake_grant" "schema_grant" {
+  database_name = "TF_DEMO"
+  schema_name   = "TF_DEMO"
+  privilege     = "USAGE"
+  roles         = ["TF_DEMO_SVC_ROLE"]
 }
 
-resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
-
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_account_object {
-    object_type = "WAREHOUSE"
-    object_name = snowflake_warehouse.warehouse.name
-  }
-}
-
-resource "tls_private_key" "svc_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "snowflake_user" "user" {
- 
-    name              = "tf_demo_user"
-    default_warehouse = snowflake_warehouse.warehouse.name
-    default_role      = snowflake_role.role.name
-    default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-}
-
-resource "snowflake_grant_privileges_to_account_role" "user_grant" {
-
-  privileges        = ["MONITOR"]
-  account_role_name = snowflake_role.role.name  
-  on_account_object {
-    object_type = "USER"
-    object_name = snowflake_user.user.name
-  }
-}
-
-resource "snowflake_grant_account_role" "grants" {
-
-  role_name = snowflake_role.role.name
-  user_name = snowflake_user.user.name
-}
 
